c878f2807bf482f03ceec147876be1a1ec099e3f
Michal G <33378775+Galajda@users.noreply.github.com>
2018-02-27 21:11:34 -0600

    From python.org tutorial

---
+++ simple_gui.py
@@ -0,0 +1,25 @@
+import tkinter as gui
+
+# copied from https://docs.python.org/3.6/library/tkinter.html
+class Application(gui.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = gui.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = gui.Button(self, text="QUIT", fg="red",
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = gui.Tk()
+app = Application(master=root)
+app.mainloop()
